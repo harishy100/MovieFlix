8b23d2c08c0de8f18c724f47a36bb37281243029
harishy100 <harishyalamanchili@gmail.com>
2017-02-27 15:05:46 -0500

    Update UserController.java
    Swagger update

--- Server/src/main/java/sample/project1/controller/UserController.java
+++ Server/src/main/java/sample/project1/controller/UserController.java
@@ -2,6 +2,7 @@ package sample.project1.controller;
 import java.util.List;
 
 
+
 import sample.project1.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,15 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
 import sample.project1.entity.User;
 import sample.project1.service.*;
 
 @RestController
 
 @RequestMapping(value= "user")
-
+@Api(tags="user")
 public class UserController {
 
 		@Autowired
@@ -31,6 +34,10 @@ public class UserController {
 			return service.login(email,password);
 		}
 		@RequestMapping(method=RequestMethod.POST)
+		@ApiOperation(value = "Create User", notes = "Creates a user in the app with unique email")
+		@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+				@ApiResponse(code = 400, message = "Bad Request"),
+				@ApiResponse(code = 500, message = "Internal Server Error"), })
 		public User create(@RequestBody User usr){
 			return service.create(usr);	
 		}
